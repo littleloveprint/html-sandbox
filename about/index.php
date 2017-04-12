6fd602dd9a3bb1cc01c966bc229e48d904cb934e
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-04-12 09:37:45 -0600

    Added table and lists.

---
+++ about/index.php
@@ -0,0 +1,8 @@
+This is the about page
+<?php
+/**
+ * Created by PhpStorm.
+ * User: littleloveprint
+ * Date: 4/12/17
+ * Time: 9:01 AM
+ */
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 	<head>
 		<meta charset="utf-8">
 
@@ -13,5 +13,57 @@
 	</head>
 	<body>
 
+		<header>
+			<h1>HTML Sandbox</h1>
+		</header>
+
+		<main>
+			<h2>Commander Riker Says:</h2>
+			<p>Talk about going nowhere fast. I'd like to think that I haven't changed those things, sir. Mr. Crusher, ready a collision course with the Borg ship. I recommend you don't fire until you're within 40,000 kilometers. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. The unexpected is our normal routine. What's a knock-out like you doing in a computer-generated gin joint like this?</p>
+			<p>Talk about going nowhere fast. I'd like to think that I haven't changed those things, sir. Mr. Crusher, ready a collision course with the Borg ship. I recommend you don't fire until you're within 40,000 kilometers. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. The unexpected is our normal routine. What's a knock-out like you doing in a computer-generated gin joint like this?</p>
+			<img src="img/star-trek-cat.png" alt="Star Trek Cat" />
+			<p>This is a link to Robbie's favorite site: <a href="https://www.espn.com" target="_blank">ESPN</a ></p>
+			<p>This is a link to an internal page: <a href="about/index.php">About Page</a> </p>
+
+			<h2>A few of my favorite things:</h2>
+			<ul>
+				<li>Tacos</li>
+				<li>Animals</li>
+				<li>Netflix</li>
+				<li>Sleep</li>
+			</ul>
+
+			<h2>A few of Sebastians Favorite Things:</h2>
+			<ol>
+				<li>Spider Man</li>
+				<li>Albuquerque</li>
+				<li>Trolls</li>
+			</ol>
+
+			<h2>Cats &amp; Dogs</h2>
+			<table>
+				<tr>
+					<th>Cats</th>
+					<th>Dogs</th>
+				</tr>
+				<tr>
+					<td>Tabby</td>
+					<td>Bulldog</td>
+				</tr>
+				<tr>
+					<td>Persian</td>
+					<td>Beagle</td>
+				</tr>
+				<tr>
+					<td>Senator</td>
+					<td>Janitor</td>
+				</tr>
+				<tr>
+					<td>Grumpy</td>
+					<td>Happy</td>
+				</tr>
+			</table>
+
+		</main>
 	</body>
 </html>
